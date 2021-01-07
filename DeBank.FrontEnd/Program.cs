9681e09c1fcadaaf7306cacc2c055bc8db7da00a
Flonja <vanjavanessen@icloud.com>
2021-01-07 12:56:22 +0100

    Edited FrontEnd & Added tests

--- DeBank.FrontEnd/Program.cs
+++ DeBank.FrontEnd/Program.cs
@@ -2,14 +2,24 @@
 using DeBank.Library.Logic;
 using DeBank.Library.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DeBank.FrontEnd
 {
+    internal struct TransferMoneyStruct
+    {
+        public BankAccount Account1 { get; set; }
+        public BankAccount Account2 { get; set; }
+
+        public List<User> Users { get; set; }
+    }
+
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             using (var db = new BankDbContext())
             {
@@ -25,24 +35,37 @@ namespace DeBank.FrontEnd
                         Console.WriteLine("     Money: " + account.Money);
                     }
                 }
+                
+                User user1 = db.Users.Where(u => u.Name == "Vanja van Essen").FirstOrDefault();
+                User user2 = db.Users.Where(u => u.Name == "Luna Herder").FirstOrDefault();
 
-                var user1 = db.Users.Where(u => u.Name == "Vanja van Essen").FirstOrDefault();
-                var user2 = db.Users.Where(u => u.Name == "Luna Herder").FirstOrDefault();
+                BankAccount account1 = user1.Accounts.FirstOrDefault();
+                BankAccount account2 = user2.Accounts.FirstOrDefault();
 
-                var account1 = user1.Accounts.FirstOrDefault();
-                var account2 = user2.Accounts.FirstOrDefault();
+                Thread transferMoneyThread = new Thread(TransferMoney);
+                transferMoneyThread.Start(new TransferMoneyStruct { Account1 = account1, Account2 = account2, Users = db.Users.ToList() });
+            }
+        }
 
-                await BankLogic.TransferMoney(account1, account2, 10, "Testen");
+        private static async void TransferMoney(object obj)
+        {
+            if(!(obj is TransferMoneyStruct))
+            {
+                return;
+            }
 
-                Console.WriteLine("Users (AFTER):");
-                foreach (User user in db.Users)
+            TransferMoneyStruct strct = (TransferMoneyStruct) obj;
+
+            await BankLogic.TransferMoney(strct.Account1, strct.Account2, 10, "Testen");
+
+            Console.WriteLine("Users (AFTER):");
+            foreach (User user in strct.Users)
+            {
+                Console.WriteLine("  " + user.Name + ":");
+                foreach (BankAccount account in user.Accounts)
                 {
-                    Console.WriteLine("  " + user.Name + ":");
-                    foreach (BankAccount account in user.Accounts)
-                    {
-                        Console.WriteLine("  - " + account.Name + ":");
-                        Console.WriteLine("     Money: " + account.Money);
-                    }
+                    Console.WriteLine("  - " + account.Name + ":");
+                    Console.WriteLine("     Money: " + account.Money);
                 }
             }
         }
---
+++ DeBank.Tests/MoneyTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DeBank.Library.Models;
+using DeBank.Library.Logic;
+
+namespace DeBank.Tests
+{
+    [TestClass]
+    public class MoneyTests
+    {
+        [TestMethod]
+        public void AddMoneyTest()
+        {
+            List<BankAccount> accounts = new List<BankAccount>();
+            accounts.Add(new BankAccount { Name = "Persoonlijke Kaart" });
+
+            User user1 = new User { Name = "Vanja van Essen", Accounts = accounts };
+
+            BankLogic.AddMoney(user1.Accounts[0], 10, "Initial money").Wait();
+
+            Assert.AreEqual(10, user1.Accounts[0].Money, "Account didn't get the money correctly");
+        }
+
+        [TestMethod]
+        public void SpendMoneyOnceTest()
+        {
+            List<BankAccount> accounts = new List<BankAccount>();
+            accounts.Add(new BankAccount { Name = "Persoonlijke Kaart" });
+
+            User user1 = new User { Name = "Vanja van Essen", Accounts = accounts };
+
+            BankLogic.AddMoney(user1.Accounts[0], 500, "Initial money").Wait();
+
+            BankLogic.SpendMoney(user1.Accounts[0], 10).Wait();
+
+            Assert.AreEqual(490, user1.Accounts[0].Money, "Account didn't remove the money correctly");
+        }
+
+        [TestMethod]
+        public void SpendMoneyTwiceTest()
+        {
+            List<BankAccount> accounts1 = new List<BankAccount>();
+            accounts1.Add(new BankAccount { Name = "Persoonlijke Kaart" });
+
+            User user1 = new User { Name = "Vanja van Essen", Accounts = accounts1 };
+
+            BankLogic.AddMoney(user1.Accounts[0], 250, "Initial money").Wait();
+
+            BankLogic.SpendMoney(user1.Accounts[0], 10).Wait();
+
+            BankLogic.SpendMoney(user1.Accounts[0], 10, true).Wait();
+            user1.Accounts[0].PreviousTransactions.FindLast(transaction => transaction.MayExecuteMore == true).Queue().Wait();
+
+            Assert.AreEqual(220, user1.Accounts[0].Money, "Account didn't remove the money correctly");
+        }
+    }
+}
